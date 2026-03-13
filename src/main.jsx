a6874e0bfc3df60a64448129c9297df4a411ea12
CC Feng <101470046+cecilianfeng@users.noreply.github.com>
2026-03-12 22:49:27 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
-import { AuthProvider } from './context/Auth
+import { AuthProvider } from './context/AuthContext'
+import App from './App'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
